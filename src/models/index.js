56e1061f7a84de26df186954c89d716a0ebf5ca0
Felipe Ramos <felipe.ramos@osf.digital>
2023-03-19 10:31:24 -0300

    [#35] Create the schema and models for pull_requrest collection
    Created the schema and models for pull_requrest collection

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,15 @@
 const routes = require('./routes');
 const Project = require('./database/Project');
 const Task = require('./database/Task');
+const PullRequest = require('./database/PullRequest');
+const Ticket = require('./database/Ticket');
+const User = require('./database/User');
 
 module.exports = {
     routes,
     Project,
-    Task
+    Task,
+    PullRequest,
+    Ticket,
+    User
 };
---
+++ src/schemas/class/PullRequestsClass.js
@@ -0,0 +1,7 @@
+const PullRequest = require('../../models/database/PullRequest');
+
+class PullRequestsClass {
+    static Model = PullRequest;
+}
+
+module.exports = PullRequestsClass;
