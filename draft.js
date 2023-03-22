ae64c94aed5022e4153cd743b2494a9322c32da3
Felipe Ramos <felipe.ramos@osf.digital>
2023-03-22 07:19:19 -0300

    [#52] Create the basics methods
    - Pre-create a checkout method
    - Pre-create a addChanges method
    - Pre-create a commit method
    - Pre-create a push method

--- draft.js
+++ draft.js
@@ -1,1 +1,12 @@
 require('./src/global');
+
+const RepoManager = require('./src/services/GitHubAPI/RepoManager');
+console.log('RepoManager', RepoManager);
+// const repo = new RepoManager({
+//     repoName: 'FelipeRamosLab/nodejs-lab',
+//     localPath: 'C:/Users/Felipe/Documents/my-lab/nodejs-lab'
+// });
+
+// repo.createBranch('feature/SBBW-225-v2', 'support').then(response => {
+//     console.log(response)
+// });
