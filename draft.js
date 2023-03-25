0dea285161be309a59afd2ba53af68b33fdc58d2
FelipeRamos1202 <felipe.ramos@osf.digital>
2023-03-25 08:47:23 -0300

    [#63] Create methods to render primitive types
    Created the methods:
    - string
    - array
    - date

--- draft.js
+++ draft.js
@@ -1,11 +1,3 @@
 require('./src/global');
 // Initializing MongoDB
 require('./src/services/database/init');
-
-const config = require('./config.json');
-const Component = require('./src/interface/Component');
-
-(()=>{
-
-})()
-
