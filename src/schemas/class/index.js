9aa54df09f69b944bced28a9367c238ab6aa7e1e
Felipe Ramos <86696152+FelipeRamos1202@users.noreply.github.com>
2023-03-25 03:26:42 -0300

    [#59] Create components schema base for the future (#61)
    Created components' schema base for the future

--- src/schemas/class/index.js
+++ src/schemas/class/index.js
@@ -3,11 +3,21 @@ const logs = require('./LogsClass');
 const users = require('./UsersClass');
 const tickets = require('./TicketsClass');
 const comments = require('./CommentsClass');
+const projects = require('./ProjectsClass');
+const pull_requests = require('./PullRequestsClass');
+const repos = require('./ReposClass');
+const components = require('./ComponentsClass');
+const tasks = require('./TasksClass');
 
 module.exports = {
     GlobalClass,
     logs,
     tickets,
     comments,
+    projects,
+    pull_requests,
+    repos,
+    components,
+    tasks,
     users
 };
