585d278fb3293598d5613db384233c3a1de3d94b
FelipeRamos1202 <felipe.ramos@osf.digital>
2023-03-25 04:52:39 -0300

    [#60] Create a Component base instance
    Created the base for a Component instance

--- draft.js
+++ draft.js
@@ -1,15 +1,11 @@
 require('./src/global');
-const config = require('./config.json');
-const { RepoManager } = require('./src/services/GitHubAPI');
-const repo = new RepoManager({
-    userName: 'FelipeRamos1202',
-    ...config.projects[0].repos[0]
-});
+// Initializing MongoDB
+require('./src/services/database/init');
 
-(async () => {
-    // const branch = await repo.checkout('feature/testing1');
-    // const fileChanges = await repo.currentChanges();
-    // const commit = await repo.commit();
-    // const push = await repo.push('feature/testing1');
+const config = require('./config.json');
+const Component = require('./src/interface/Component');
+
+(()=>{
+
 })()
 
