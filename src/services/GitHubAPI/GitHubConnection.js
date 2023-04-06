0d2e937c16a91c345185534c97ab2825412a054b
Felipe Ramos <86696152+FelipeRamos1202@users.noreply.github.com>
2023-04-06 18:24:22 -0300

    [#131] Create method to get the current branch (#132)
    Create method to get the current branch

--- src/services/GitHubAPI/GitHubConnection.js
+++ src/services/GitHubAPI/GitHubConnection.js
@@ -1,11 +1,13 @@
 const config = require('../../../config.json');
+const GitHubUser = require('./GitHubUser');
 
-class GitHubConnection {
+class GitHubConnection extends GitHubUser {
     constructor (setup = {
         userName: '',
         GITHUB_USER_TOKEN: '',
         organization: ''
     }) {
+        super(setup);
         const { GITHUB_USER_TOKEN, userName, organization } = setup || {};
 
         this.getGITHUB_USER_TOKEN = () => GITHUB_USER_TOKEN || process.env.GITHUB_USER_TOKEN;
