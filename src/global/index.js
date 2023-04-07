51d8802fdc55cc0635d6df2fcb6146bca54d2d23
FelipeRamos1202 <felipe.ramos@osf.digital>
2023-04-07 00:40:18 -0300

    [#135] Create method to create a new branch
    - Created method RepoManager.createBranch
    - Created method RepoManager.stash
    - Created method RepoManager.applyStash
    - Improved and updated the method RepoManager.checkout

--- src/global/index.js
+++ src/global/index.js
@@ -3,10 +3,13 @@ const ResourceCenter = require('../resources/Resources');
 const validation = require('../validation');
 const ErrorLog = require('../models/logs/ErrorLog');
 const configs = require('../../config.json');
+const ToolsCLI = require('../interface/CLI/ToolsCLI');
+const toolsCLI = new ToolsCLI();
 
 global.ajax = ajax;
 // Resources
 global.Resource = new ResourceCenter(configs.defaultLanguage);
+global.toolsCLI = toolsCLI;
 
 // Declarations
 global.Error.Log = ErrorLog;
