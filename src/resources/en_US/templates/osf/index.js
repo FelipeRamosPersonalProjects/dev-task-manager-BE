31bc8c193f263d97b6574e100e402101ec683eb7
FelipeRamos1202 <felipe.ramos@osf.digital>
2023-04-12 18:10:28 -0300

    [#159] Resources templates improvements and modifications
    Resources templates improvements and modifications

--- src/resources/en_US/templates/osf/index.js
+++ src/resources/en_US/templates/osf/index.js
@@ -1,58 +1,30 @@
-const fs = require('fs');
 const Component = require('../../../../interface/Component');
 const StringTemplateBuilder = require('../../../../interface/StringTemplateBuilder');
+const DefaultPRDescription = require('./default_pr_description');
 
 module.exports = {
-    default_branch_name: () => {
-        return new Component({
+    default_branch_name: async () => {
+        return await new Component({
             componentName: 'OSF default branch name',
             description: 'The main template used by OSF for branch names.',
-            outputModel: new StringTemplateBuilder().text('feature/').var('taskID', 'string').end()
-        });
+            outputModel: new StringTemplateBuilder().var('taskBranch', 'string').end()
+        }).init();
     },
 
-    default_pr_title: () => {
-        return new Component({
+    default_pr_title: async () => {
+        return await new Component({
             componentName: 'OSF pull request title',
             description: 'The main template used by OSF for PR titles',
             outputModel: new StringTemplateBuilder()
                 .text('[').var('taskID', 'string').text('] : ').var('taskTitle', 'string')
             .end()
-        });
+        }).init();
     },
 
     default_pr_description: async () => {
-        return new Promise((resolve, reject) => {
-            fs.readFile('src/resources/en_US/templates/osf/assets/default_pr.md', (err, data) => {
-                if (err) {
-                    reject(err);
-                }
-
-                const template = data.toString();
-                const component = new Component({
-                    componentName: 'OSF pull request description',
-                    description: 'The main template used by OSF for PR descriptions.',
-                    outputModel: template,
-                    types: {
-                        FileChange:  new Component({
-                            outputModel: new StringTemplateBuilder()
-                                .text(`#### `).var('filename', 'string').text(`:`).newLine()
-                            .end()
-                        })
-                    }
-                });
-
-                resolve(component);
-            });
-        });
-    },
-
-    default_filechange_item: () => {
-        return new Component({
-            outputModel: new StringTemplateBuilder()
-                .text('- **').var('filename', 'string').text(`**:`).newLine()
-                .text('_').var('changeDescription', 'string').text('_').newLine()
-            .end()
-        });
+        return await new DefaultPRDescription({
+            componentName: 'OSF pull request description',
+            description: 'The main template used by OSF for PR descriptions.'
+        }).init();
     }
 };
