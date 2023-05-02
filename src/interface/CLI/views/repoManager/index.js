b2568ab1c97cab62c0ca60e9236cf5240396db27
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2023-05-01 21:03:45 -0300

    [DESK-33] Create a Pull CLI view under Repo Manager menu (#239)
    [DESK-33] Created a Pull CLI view under Repo Manager menu
    
    Co-authored-by: FelipeRamos1202 <felipe.ramos@osf.digital>

--- src/interface/CLI/views/repoManager/index.js
+++ src/interface/CLI/views/repoManager/index.js
@@ -1,9 +1,11 @@
 const fetch = require('./fetch');
 const commit = require('./commit');
 const push = require('./push');
+const pull = require('./pull');
 
 module.exports = {
     fetch,
     commit,
-    push
+    push,
+    pull
 };
