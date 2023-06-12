b3cbc5bd4bc44e6f34fbd48f1baf82a8472c312a
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2023-06-12 04:33:08 -0300

    [DESK-99] Create HTTPS server (#296)
    * [DESK-99] Create HTTPS server
    
    * [DESK-99] Fixing URL path
    
    * [DESK-99] Fixing variable
    
    * [DESK-99] Adding Test Route
    
    * [DESK-99] FIxing issue on tasks schema
    
    ---------
    
    Co-authored-by: FelipeRamos1202 <felipe.ramos@osf.digital>

--- server.js
+++ server.js
@@ -1,23 +1,53 @@
 require('module-alias/register');
+
 const express = require('express');
+const https = require('https');
 const app = express();
 const cors = require('cors');
+const fs = require('fs');
 
 // Declaring globals
 require('./src/global');
+
 // Initializing MongoDB
-require('./src/services/database/init');
+require('@services/database/init').then(async () => {
+    // Importing Routes
+    const routes = require('./src/routes');
 
-// Importing Routes
-const routes = require('./src/routes');
+    // Configuring server
+    app.use(cors());
+    app.use(express.json());
 
-// Configuring server
-app.use(cors());
-app.use(express.json());
+    // Server routes
+    app.use('/collection', routes.collection);
+    app.get('/health-check', async (req, res) => {
+        res.status(200).end('API Health: OK');
+    });
 
-// Server routes
-app.use('/collection', routes.collection);
+    if (process.env.ENV_NAME === 'STG' || process.env.ENV_NAME === 'PROD') {
+        const SSL_KEY = fs.readFileSync(__dirname + '/cert/ca.key');
+        const SSL_CERT = fs.readFileSync(__dirname + '/cert/ca.crt');
 
-app.listen(80, ()=>{
-    console.log('Server connected in http://localhost:80');
+        if (SSL_KEY && SSL_CERT) {
+            const options = {
+                key: SSL_KEY,
+                cert: SSL_CERT
+            };
+
+            https.createServer(options, app).listen(Number(process.env.PORT_HTTPS), () => {
+                console.log(`Server listening on port ${process.env.PORT_HTTPS}`);
+            });
+        } else {
+            throw new Error.Log({
+                name: 'SSLCertificateNotFound',
+                message: `The SSL certificate wasn't found on the directory!`
+            });
+        }
+    } else {
+        app.listen(Number(process.env.PORT_HTTP), ()=>{
+            console.log('Server connected in http://localhost:80');
+        });
+    }
+}).catch(err => {
+    throw err;
 });
