0fc1725c97f8b173d89c2f5d172dff67fb425200
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2023-06-12 22:47:34 -0300

    [DESK-107] Implement the login controller (#300)
    Co-authored-by: FelipeRamos1202 <felipe.ramos@osf.digital>

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,4 +1,30 @@
-module.exports = async (req, res) => {
-    
-}
+const Request = require('@models/RequestAPI');
+const User = require('@models/collections/User');
 
+const bodySchema = {
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+};
+
+module.exports = async function (req, res) {
+    try {
+        const request = new Request(req, bodySchema);
+        const body = request.getBody();
+        const user = await User.signIn(body.email, body.password);
+
+        if (user instanceof Error.Log) {
+            let status = 500;
+
+            if (user.name === 'AUTH_INVALID_CREDENTIALS') {
+                status = 401;
+            }
+
+            return res.status(status).send(user.toJSON());
+        }
+
+        const response = user.toPublic({token: user.token});
+        return res.status(200).send(response.toSuccess().toJSON());
+    } catch(err) {
+        return res.status(500).send(new Error.Log(err).toJSON());
+    }
+}
