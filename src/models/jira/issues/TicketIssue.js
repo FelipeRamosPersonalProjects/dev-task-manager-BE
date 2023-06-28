61c4812c30d2c51815e0c27e21d86ccb98bf4e42
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2023-06-28 06:32:34 -0300

    [DESK--134] Implement TICKETs custom fields (#326)
    Co-authored-by: FelipeRamos1202 <felipe.ramos@osf.digital>

--- src/models/jira/issues/TicketIssue.js
+++ src/models/jira/issues/TicketIssue.js
@@ -5,9 +5,13 @@ class JIRATicketIssue extends JIRAIssue {
         super(setup);
 
         try {
-            const { externalKey } = Object(setup);
+            const { priorityIndex, externalKey, externalURL, infoDisplay, mdHTML } = Object(setup);
 
-            this.fields.customfield_10085 = new JIRAIssue.JIRAFields(externalKey).text();
+            this.fields.customfield_10094 = new JIRAIssue.JIRAFields(priorityIndex || 0).number();
+            this.fields.customfield_10096 = new JIRAIssue.JIRAFields(externalKey).text();
+            this.fields.customfield_10095 = new JIRAIssue.JIRAFields(externalURL).text();
+            this.fields.customfield_10093 = new JIRAIssue.JIRAFields(infoDisplay).text();
+            this.fields.customfield_10091 = new JIRAIssue.JIRAFields(mdHTML).text();
         } catch (err) {
             throw new Error.Log(err);
         }
